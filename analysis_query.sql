6dcac50fd4c6f157e14b55edd55043c17b8b6b21
tonitiri <taiwo@beyondbordersinvestment.com>
2019-09-15 23:56:11 -0500

    homework 90%

--- analysis_query.sql
+++ analysis_query.sql
@@ -36,6 +36,67 @@ FROM
    department_managers
 	LEFT JOIN departments ON department_managers.dept_no = departments.dept_no
 		LEFT JOIN employees ON department_managers.emp_no = employees.emp_no;
+		
+--List the department of each employee with the following information: 
+--employee number, last name, first name, and department name.
+SELECT employees.emp_no,
+  employees.last_name,
+  employees.first_name,
+  departments.dept_name
+  
+FROM employees
+		LEFT JOIN department_employees ON employees.emp_no = department_employees.emp_no
+		LEFT JOIN departments ON department_employees.dept_no = departments.dept_no;
+
+--List all employees whose first name is "Hercules" and last names begin with "B."   
+SELECT
+   first_name,
+        last_name
+FROM
+   employees
+WHERE
+   first_name = 'Hercules'
+AND last_name LIKE 'B%';
+
+--List all employees in the Sales department, including their employee number, last name, first name, and department name.
+SELECT employees.emp_no,
+  employees.last_name,
+  employees.first_name,
+  departments.dept_name
+  
+		FROM employees
+		LEFT JOIN department_employees ON employees.emp_no = department_employees.emp_no
+		LEFT JOIN departments ON department_employees.dept_no = departments.dept_no
+
+WHERE
+   departments.dept_name ='Sales';
+   
+--List all employees in the Sales and Development departments, including their 
+--employee number, last name, first name, and department name.
+SELECT employees.emp_no,
+  employees.last_name,
+  employees.first_name,
+  departments.dept_name
+  
+		FROM employees
+		LEFT JOIN department_employees ON employees.emp_no = department_employees.emp_no
+		LEFT JOIN departments ON department_employees.dept_no = departments.dept_no
+
+WHERE
+   departments.dept_name ='Sales' 
+OR departments.dept_name ='Development';
+
+--In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
+SELECT
+   last_name,
+   COUNT (last_name)
+FROM
+   employees
+GROUP BY
+   last_name
+ORDER BY
+   "count" DESC;
+
 
 
 
